1cde201100c05c6a66e269a7b4bcd7f245a46e08
Tk-Sb <pmn4222@gmail.com>
2024-07-06 11:40:26 +0300

    cleaning up

--- app/lib/db.js
+++
@@ -1,7 +0,0 @@
-import { neon } from "@neondatabase/serverless"
-
-export async function getData() {
-    const sql = neon(process.env.DATABASE_URL)
-    const data = await sql`INSERT INTO students (student_firstname, student_lastname, student_class) VALUES (tk, sb, 100);`
-    return data
-}
--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,8 @@
-import getDomain from "./lib/domain";
-import getData  from "./lib/db";
+
 export default function Home() {
-  console.log("Domain is:", getDomain())
-  console.log(getData())
+
   return (
     <>
-    
     </>
   );
 }
